b6e685e2f0a9910e90638a82ecaebbb668e329bf
Joe Van Gundy <joevangundy@joes-mbp.home>
2020-04-26 15:29:17 -0400

    Forever spins. Player synced.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject ObjectToRotateAround;
+    public int rotateSpeed;
+    public float jumpVelocity = 5;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+    public LayerMask groundLayers;
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = transform.GetComponent<Rigidbody>();
+    }
+
+    // private bool IsGrounded()
+    // {
+    //     CapsuleCollider col = GetComponent<CapsuleCollider>();
+    //     return Physics.CheckCapsule(col.bounds.center, new Vector3(col.bounds.center.x, col.bounds.min.y, col.bounds.center.z), col.radius * 0.9f, groundLayers);
+    // }
+
+    void FixedUpdate() {
+        transform.RotateAround(new Vector3(0,0,0), Vector3.down, rotateSpeed * Time.deltaTime);
+        if(Input.GetButtonDown("Jump")) {
+            rb.velocity = Vector3.up * jumpVelocity;
+        }
+        if(rb.velocity.y < 0) {
+            rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        } else if (rb.velocity.y > 0 && !Input.GetButton("Jump")){
+            rb.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+
+    void Update()
+    {
+    }
+
+
+    // Update is called once per frame
+    // void FixedUpdate()
+    // {
+    //     // transform.RotateAround(new Vector3(0,0,0), Vector3.down, rotateSpeed * Time.deltaTime);
+
+    //     // if(Input.GetKeyDown(KeyCode.Space) && IsGrounded()) {
+    //     //     rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    //     // }
+    // }
+}
---
+++ Assets/Scripts/SpiralSpawner.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SpiralSpawner : MonoBehaviour
+{
+    public GameObject spiral;
+    public Queue<GameObject> spirals = new Queue<GameObject>();
+    public float yRotation = 0;
+    public int numberOfSpirals = 5;
+    void Start()
+    {
+        float repeatTime = spiral.transform.Find("Pillar").GetComponent<Renderer>().bounds.size.y/5;
+        // Hack: Invoke repeating was off by one frame. So wait one less frame than normal to start
+        InvokeRepeating("SpawnSpiral", repeatTime-Time.deltaTime, repeatTime);
+        
+    }
+    void Awake() {
+         SetupInitialSpirals();
+    }
+    void SetupInitialSpirals()
+    {
+        for (int i=numberOfSpirals; i > 0; i--)
+        {
+            GameObject spawned = Instantiate(spiral, new Vector3(0, (-10*i) + 5, 0), Quaternion.identity);
+            Rigidbody rb = spawned.GetComponent<Rigidbody>();
+            rb.velocity = new Vector3(0, -5, 0);
+            spawned.transform.eulerAngles = new Vector3(
+                spawned.transform.eulerAngles.x,
+                yRotation,
+                spawned.transform.eulerAngles.z
+            );
+            spirals.Enqueue(spawned);
+        }
+    }
+
+    void SpawnSpiral()
+    {
+        GameObject spawned = Instantiate(spiral, new Vector3(0, -5, 0), Quaternion.identity);
+        Rigidbody rb = spawned.GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(0, -5, 0);
+        spawned.transform.eulerAngles = new Vector3(
+            spawned.transform.eulerAngles.x,
+            yRotation,
+            spawned.transform.eulerAngles.z
+        );
+        spirals.Enqueue(spawned);
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        foreach (GameObject obj in spirals)
+        {
+            obj.transform.eulerAngles = new Vector3(
+                obj.transform.eulerAngles.x,
+                yRotation,
+                obj.transform.eulerAngles.z
+            );
+        }
+        if(yRotation >= 360) {
+            yRotation = 0;
+        }
+        yRotation += 1;
+
+        if(spirals.Count >= numberOfSpirals) {
+            GameObject obj = spirals.Dequeue();
+            Destroy(obj);
+        }
+    }
+}
